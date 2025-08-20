907287a2594eefb015b8ad1238f68d8397977942
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2025-08-20 10:07:51 -0500

    Improve the instructions in the lab

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("You can create a driver program for the converter here!");
+    System.out.println("Write a driver program for the Converter class here!");
+    System.out.println("You can delete these print statements and replace them with your code.");
   }
 }
