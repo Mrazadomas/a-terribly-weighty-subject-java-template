cb8be749a901193fe09e5e40345d75e958b48994
Mrazadomas <117606034+Mrazadomas@users.noreply.github.com>
2025-08-24 18:25:12 -0500

    done

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,8 +1,22 @@
 package org.example;
+import java.util.Scanner;
 
 public class App {
+  
   public static void main(String[] args) {
-    System.out.println("Write a driver program for the Converter class here!");
-    System.out.println("You can delete these print statements and replace them with your code.");
+    System.out.println("Welcome to the Terribly Accurate Converter App!");
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter your number of ounces: ");
+    int num = in.nextInt();
+    if (!(num == (int)num)) {
+        System.out.println("Please enter an integer...");
+    } else if (num < 0) {
+        System.out.println("Please enter a positive integer...");
+    } else {
+        System.out.println(Converter.toPounds(num));
+        System.out.println(Converter.toPoundsAndOunces(num));
+    }
+    in.close();
+    
   }
 }
--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -1,4 +1,7 @@
 package org.example;
+import java.text.DecimalFormat;
+
+   
 
 public class Converter {
   // Write the code needed to make the tests in
@@ -11,4 +14,30 @@ public class Converter {
   // pass, exercise your code in a driver function in
   // App.java. This shows that your business logic can
   // run in a console program.
+
+
+
+    public static String toPounds(int oz) {
+        double pound = (double)oz / 16;
+        DecimalFormat df = new DecimalFormat("0.0000"); 
+        String pounds = df.format(((double)oz) / 16);
+        if (pound == 1) {
+            String resultS = pounds + " lb";
+            return resultS;
+        }
+        String result = pounds + " lbs";
+        return result;
+    }
+
+    public static String toPoundsAndOunces(int oz) {
+        double pound = (double)oz / 16;
+        int pounds = oz / 16;
+        int ozLeft = oz % 16;
+        if (pound >= 1 && pound < 2) {
+            String resultS = pounds + " lb " + ozLeft + " oz";
+            return resultS;
+        }
+        String result = pounds + " lbs " + ozLeft + " oz";
+        return result;
+    }
 }
